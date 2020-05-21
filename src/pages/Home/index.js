dcc1d90264f314b5d71e77f1d6f9ab93b8ff9d47
Marcelo Pinheiro Meireles <marcelopimeireles@gmail.com>
2020-05-21 12:35:01 +0100

    home styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,88 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import product from '../../assets/images/product.jpg';
+
+import { ProductList } from './styles';
 
 function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img src={product} alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={product} alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={product} alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={product} alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={product} alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img src={product} alt="Tênis" />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
 
 export default Home;
